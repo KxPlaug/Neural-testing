b56f9aabcc799c89e0a5bffd4adbed9fdcfb0d0b
KxPlaug <3175389813@qq.com>
2023-06-09 08:59:20 +0800

    update

--- Config/Dataset/example.py
+++ Config/Dataset/example.py
@@ -8,13 +8,19 @@ def load_dataset():
     """Load Custom dataset."""
     ...
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    x_batch = torch.load(os.path.join(
-        parent_dir, 'imagenet_1k/imagenet_x_batch.pt'), map_location=torch.device('cpu'))[:20]
-    y_batch = torch.load(os.path.join(
-        parent_dir, 'imagenet_1k/imagenet_y_batch.pt'), map_location=torch.device('cpu'))[:20]
-    x_batch = Resize((224, 224))(x_batch)
-    dataset = TensorDataset(x_batch, y_batch)
-    return train_dataset,test_dataset
+    x_train_batch = torch.load(os.path.join(
+        parent_dir, 'imagenet_1k/train/imagenet_x_batch.pt'), map_location=torch.device('cpu'))
+    y_train_batch = torch.load(os.path.join(
+        parent_dir, 'imagenet_1k/train/imagenet_y_batch.pt'), map_location=torch.device('cpu'))
+    x_test_batch = torch.load(os.path.join(
+        parent_dir, 'imagenet_1k/test/imagenet_x_batch.pt'), map_location=torch.device('cpu'))
+    y_test_batch = torch.load(os.path.join(
+        parent_dir, 'imagenet_1k/test/imagenet_y_batch.pt'), map_location=torch.device('cpu'))
+    x_train_batch = Resize((224, 224))(x_train_batch)
+    x_test_batch = Resize((224, 224))(x_test_batch)
+    train_dataset = TensorDataset(x_train_batch, y_train_batch)
+    test_dataset = TensorDataset(x_test_batch, y_test_batch)
+    return train_dataset, test_dataset
 
 
 if __name__ == "__main__":
